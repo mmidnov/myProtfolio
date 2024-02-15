ac4903d9b06b8781c3a752479d292530636b98fd
mmidinov <mmidinov13@gmail.com>
2024-02-15 19:10:48 +0600

    portfolio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,47 +1,47 @@
-import hero from '../../assets/munar.png'
-import react from '../../assets/react.png'
-import java from '../../assets/javascript.png'
-import html from '../../assets/html.png'
-import css from '../../assets/css.png'
-import './Hero.css'
+import hero from "../../assets/munar.png";
+import react from "../../assets/react.png";
+import java from "../../assets/javascript.png";
+import html from "../../assets/html.png";
+import css from "../../assets/css.png";
+import "./Hero.css";
 const Hero = () => {
   return (
-    <section className='hero-container'>
+    <section className="hero-container">
       <div className="hero-content">
         <h2>Building Digital Experiences That Inspire</h2>
         <p>
-            Passionate frontend Developer | Transforming Ideas into Seemless and Visually Stunning Web Solutions
+          Passionate frontend Developer | Transforming Ideas into Seemless and
+          Visually Stunning Web Solutions
         </p>
       </div>
 
-
       <div className="hero-img">
         <div>
-            <div className="tech-icon">
-                <img src={react} alt="" />
-            </div>
-                <img src={hero} alt="" />
+          <div className="tech-icon">
+            <img src={react} alt="" />
+          </div>
+          <img src={hero} alt="" />
         </div>
         <div>
-        <div>
+          <div>
             <div className="tech-icon">
-                <img className='htjs' src={html} alt="" />
+              <img className="htjs" src={html} alt="" />
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <div className="tech-icon">
-            <img src={css} alt="" />
+              <img src={css} alt="" />
             </div>
-        </div>
-        <div>
+          </div>
+          <div>
             <div className="tech-icon">
-            <img className='htjs' src={java} alt="" />
+              <img className="htjs" src={java} alt="" />
             </div>
-        </div>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
